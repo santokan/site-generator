44d0e997c857df8cbc847c63f473e48e34455f74
santokan <houska.peter@gmail.com>
2025-01-28 16:46:07 +0100

    added split img and split link functions

--- src/inline_md.py
+++ src/inline_md.py
@@ -23,6 +23,52 @@ def split_nodes_delimiter(old_nodes, delimeter, text_type):
     return new_nodes
 
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        images = extract_markdown_images(text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        last_index = 0
+        for alt_text, url in images:
+            start_index = text.find(f"![{alt_text}]({url})", last_index)
+            if start_index > last_index:
+                new_nodes.append(TextNode(text[last_index:start_index], TextType.TEXT))
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url=url))
+            last_index = start_index + len(f"![{alt_text}]({url})")
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        links = extract_markdown_links(text)
+        if not links:
+            new_nodes.append(node)
+            continue
+        last_index = 0
+        for link_text, url in links:
+            start_index = text.find(f"[{link_text}]({url})", last_index)
+            if start_index > last_index:
+                new_nodes.append(TextNode(text[last_index:start_index], TextType.TEXT))
+            new_nodes.append(TextNode(link_text, TextType.LINK, url=url))
+            last_index = start_index + len(f"[{link_text}]({url})")
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+    return new_nodes
+
+
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     return re.findall(pattern, text)
